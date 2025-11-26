1bb45212849e4c79a192578d4f64f40f2244ac2a
IQBAL Muhammad Asif <kar.aiqbal@cma-cgm.com>
2025-11-26 11:42:33 +0500

    Configure GitHub Pages for deployment

--- frontEnd/gen-app/vite.config.js
+++ frontEnd/gen-app/vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/OraNode03-App/', // <-- Check/Add this line
 })
